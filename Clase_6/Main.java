e5ab2d138be08d19a9204e37f0fef4d4bf638f4e
mcalistriraga <mcalistri.freel@gmail.com>
2023-08-23 14:11:31 -0400

    Crea un hola mundo

---
+++ Clase_6/Main.java
@@ -0,0 +1,8 @@
+package Clase_6;
+
+public class Main {
+    public static void main (String[] args) {
+        System.out.println("Hola mundo");
+        System.out.println("Hola Manuel!!");
+    }
+}
