bf96982f28f7b349d82efb48c244ad53e12bc748
mcalistriraga <39352240+mcalistriraga@users.noreply.github.com>
2023-08-26 18:32:26 -0400

    Create sample02-averageNumber.java

---
+++ Clase_6/sample02-averageNumber.java
@@ -0,0 +1,27 @@
+package Clase_6;
+/*
+===============================================================================================
+* EJERCICIO #2
+* “Armar un programa que permita cargar 3 números y mostrar cual es el número promedio”
+*
+===============================================================================================
+*/
+import java.util.Scanner;
+public class Main {
+    public static void main (String[] args) {
+        Scanner cargaDeDatos = new Scanner(System.in);
+
+        System.out.println("Introduce el primer número?");
+        Integer num1 = cargaDeDatos.nextInt();
+
+        System.out.println("Introduce el segundo número?");
+        Integer num2 = cargaDeDatos.nextInt();
+
+        System.out.println("Introduce el último número?");
+        Integer num3 = cargaDeDatos.nextInt();
+
+        Integer media = (num1 +num2 +num3)/3;
+
+        System.out.println("El promedio de los siguientes numeros: (" + num1 + ", " + num2 + " y " + num3 + ") es: " + media);
+    }
+}
