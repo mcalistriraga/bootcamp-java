9afbede99db53438bfd3b34fc11f21a584c37e93
mcalistriraga <39352240+mcalistriraga@users.noreply.github.com>
2023-08-28 13:50:50 -0400

    Create activ02-CelebrityTwitterSearch.java

---
+++ Clase_6/activ02-CelebrityTwitterSearch.java
@@ -0,0 +1,23 @@
+package Clase_6;
+/*
+===============================================================================================
+* ACTIVIDAD_INDIVIDUAL #2
+*     2)	Arma un buscador de tweets para Twitter.com. Es bastante similar al anterior.
+*     Pídele al usuario que ingrese el nombre de una celebridad (de una sola palabra),
+*        por ejemplo: “Messi”.
+*     Después muestra en consola la concatenación de:
+*         “https://twitter.com/search?q=” + palabra
+*===============================================================================================
+*/
+import java.util.Scanner;
+public class Main {
+    public static void main (String[] args) {
+
+        Scanner cargaDeDatos = new Scanner(System.in);
+
+        System.out.println("Introduce el NOMBRE de una celebridad ?");
+        String celebridad = cargaDeDatos.next();
+        String cmd= "https://twitter.com/search?q="+celebridad;
+        System.out.println (cmd);
+    }
+}
