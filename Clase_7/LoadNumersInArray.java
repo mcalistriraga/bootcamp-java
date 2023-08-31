b78f21f783fbe86bfbd95f8a36174ab3e30dfddc
mcalistriraga <mcalistri.freel@gmail.com>
2023-08-31 14:14:14 -0400

    Crea un hola mundo

--- Clase_7/LoadNumersInArray.java
+++ Clase_7/LoadNumersInArray.java
@@ -4,10 +4,18 @@ package Clase_7;
 *
 *    1)	Hacer un programa que permita al usuario ingresar 5 números y mostrarlos.
 *
+*     2)	Después de realizar el ejercicio anterior, agregar que muestre el
+*           número mayor y el número menor.
+*
+*     3)	Ahora, hay que mostrar el promedio. Para hacer esto se suman todos
+*           los números y se divide por 5, ya que el usuario agregó 5 números.
+*
 * *==============================================================================
 */
+import java.util.Arrays; // clase Arrays
+import java.util.OptionalInt;
 import java.util.Scanner;
-public class LoadNumersInArray {
+class LoadNumersInArray {
     public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -21,10 +29,16 @@ public class LoadNumersInArray {
 
         System.out.println("Los números ingresados son:");
 
+        //Arrays.stream(numbers).forEach(elem -> System.out.println(elem)); // new
         for (int elem: numbers)  // leer array
         {
             System.out.println(elem);
         }
 
+        System.out.println("el minimo es: " + Arrays.stream(numbers)
+                                                    .min()
+                                                    .getAsInt());
+        System.out.println("el máximo es: " + Arrays.stream(numbers).max().getAsInt());
+        System.out.println("el minimo es: " + Arrays.stream(numbers).average().getAsDouble());
     }
 }
