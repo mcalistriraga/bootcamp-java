4d240a2460d9745d55a9089ad2673646ba7b7489
mcalistriraga <39352240+mcalistriraga@users.noreply.github.com>
2023-10-27 11:46:26 -0400

    Create Usuario.java

---
+++ Clase_15/ControlUsersSystem/backend/src/main/java/academy.atl.panel/models/Usuario.java
@@ -0,0 +1,13 @@
+package academy.atl.panel.models;
+
+import lombok.*;
+
+@Data @AllArgsConstructor
+public class Usuario {
+    private Integer id;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
+
+}
