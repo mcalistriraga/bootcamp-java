85b13260c1f90c5f360211597d08e0862ef8f3af
mcalistriraga <39352240+mcalistriraga@users.noreply.github.com>
2023-11-02 13:44:02 -0400

    Update Item.java

--- Clase_16/GameEnhansed/Item.java
+++ Clase_16/GameEnhansed/Item.java
@@ -1,1 +1,29 @@
+public class Item {
+    private String nombre;
+    private int defensa; // si es una armadura, te va a dar defensa
+    private int ataque; // si es un arma, te va a dar ataque
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+}
