6d0aec916aa5f1d5fa62d4e2afaa040059c45cae
mcalistriraga <39352240+mcalistriraga@users.noreply.github.com>
2024-05-10 16:58:11 -0400

    Update EmailJob.java
    tareas para el cron

--- Clase20/newsletters/src/main/java/academy.atl.newsletters/EmailJob.java
+++ Clase20/newsletters/src/main/java/academy.atl.newsletters/EmailJob.java
@@ -1,1 +1,17 @@
+package academy.atl.newsletters;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@EnableScheduling  // ESTA ANOTACION: PERMITE EJECUTAR FUNCIONESDE NOCHE A LA HORA Q NOSOTROS QUERAMOS
+public class EmailJob {
+    // @Scheduled(cron = "0 0 3 ? * MON")  // puede servir para limpiar la base de datos, TAREAS AUTOMATIZADAS
+    //public void generarBackupDELaBaseDeDatos() {  // ojoo PEDIR A ChatGPT que lo configure OJJJJJO
+        //.....
+    //}
+
+    @Scheduled(cron = "0 0 3 ? * MON")  // se ejecuta a las 3 de la mañana. Con la conf reqierida se ejecuta cualquier dia a determinada hora
+    public void enviarCampanaDeMarketing() {
+    }
+}
 
