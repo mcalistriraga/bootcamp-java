dae7c752ca98545711dc5b95d84bffebbc7b65e5
mcalistriraga <39352240+mcalistriraga@users.noreply.github.com>
2024-05-13 18:51:51 -0400

    Update Rectangulo.java

--- Clase_25/T01_Interfaces/src/main/java/figuras/Rectangulo.java
+++ Clase_25/T01_Interfaces/src/main/java/figuras/Rectangulo.java
@@ -1,1 +1,17 @@
+package main.java.figuras;
 
+// Clase Rectangulo que implementa la interfaz FiguraGeometrica
+public class Rectangulo implements FiguraGeometrica{
+    private double base;
+    private double altura;
+
+    public Rectangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    @Override
+    public double calcularArea() {
+        return base * altura;
+    }
+}
