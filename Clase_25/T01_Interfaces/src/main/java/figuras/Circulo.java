2504f9bcf5b7847d535b933084482da3acfe3cc4
mcalistriraga <39352240+mcalistriraga@users.noreply.github.com>
2024-05-13 18:50:17 -0400

    Update Circulo.java

--- Clase_25/T01_Interfaces/src/main/java/figuras/Circulo.java
+++ Clase_25/T01_Interfaces/src/main/java/figuras/Circulo.java
@@ -1,1 +1,16 @@
+package main.java.figuras;
 
+
+// Clase Circulo que implementa la interfaz FiguraGeometrica
+public class Circulo implements FiguraGeometrica {
+    private double radio;
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+
+    @Override
+    public double calcularArea() {
+        return Math.PI * radio * radio;
+    }
+}
