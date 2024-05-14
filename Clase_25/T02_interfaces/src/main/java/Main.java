3335e9feb7fa16f46d27c2d7b848a7c7533c07a5
mcalistriraga <39352240+mcalistriraga@users.noreply.github.com>
2024-05-13 21:16:58 -0400

    Update Main.java

--- Clase_25/T02_interfaces/src/main/java/Main.java
+++ Clase_25/T02_interfaces/src/main/java/Main.java
@@ -1,1 +1,25 @@
+/*
+=================================================================================
+    Ejercicio 2: Interfaces
+    
+    Crea una interfaz llamada Musico con el método abstracto tocarInstrumento().
+    Luego, implementa la interfaz en las clases Pianista y Guitarrista.
+    En cada clase, implementa el método tocarInstrumento() para que muestre
+     un mensaje indicando qué instrumento está tocando cada músico.
+=================================================================================
+*/
+ package main.java;
 
+import main.java.musica.models.Guitarrista;
+import main.java.musica.models.Pianista;
+
+// Clase principal para probar las implementaciones
+public class Main {
+    public static void main(String[] args) {
+        Pianista pianista = new Pianista();
+        Guitarrista guitarrista= new Guitarrista();
+
+        pianista.tocarInstrumento();
+        guitarrista.tocarInstrumento();
+    }
+}
