53a4c315a67dbcc9f28d1f868bfe5db4ea5487a7
mcalistriraga <39352240+mcalistriraga@users.noreply.github.com>
2024-05-13 21:21:38 -0400

    Update Pianista.java

--- Clase_25/T02_interfaces/src/main/java/musica/models/Pianista.java
+++ Clase_25/T02_interfaces/src/main/java/musica/models/Pianista.java
@@ -1,1 +1,9 @@
+package main.java.musica.models;
+
+public class Pianista implements Musico {
+    @Override
+    public void tocarInstrumento() {
+        System.out.println("El pianista está tocando el piano.");
+    }
+}
 
